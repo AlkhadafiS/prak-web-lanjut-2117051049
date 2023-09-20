11578cad18fb4874bc2f7cba3b49c98a40cb48f1
AlkhadafiS <alkhadafi.saddam21@students.unila.ac.id>
2023-09-20 11:43:03 +0700

    tugas pertemuan 3

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -3,32 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
     <title>Document</title>
-
-    <style>
-        table {
-            width: 20%;
-            border-collapse: collapse;
-            margin: 0 auto;
-            border-radius: 10px;
-            overflow: hidden;
-        }
-
-        th, td {
-            text-align: center;
-            border: 1px solid #ccc;
-            padding: 10px;
-            background-color: #ccc;
-        }
-
-        .profile-image {
-            display: block;
-            margin: 0 auto;
-            border-radius: 50%;
-            border: 4px solid #ccc;
-        }
-    </style>
-
 </head>
 <body>
 <img class="profile-image" src="/assets/img/muka.jpg" alt="Profile Image" width="200" height="200"><br>
