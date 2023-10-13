ae6feaf94029944b16d4e76f92c50b538736e214
AlkhadafiS <alkhadafi.saddam21@students.unila.ac.id>
2023-10-13 19:13:14 +0700

    Tugas 7

--- app/Views/edit_user.php
+++ app/Views/edit_user.php
@@ -9,12 +9,13 @@
   </div>
 </nav><br>
 <div class="container">
-    <form class="container" action=<?= base_url('user/store') ?> method="POST" enctype="multipart/form-data">
-
+    <form class="container" action=<?= base_url('user/' . $user['id']) ?> method="POST" enctype="multipart/form-data">
+    <input type="hidden" name="_method" value="PUT">
+    <?= csrf_field() ?>
     <div class="mb-3 row">
             <label for="nama" class="col-sm-2 col-form-label">Nama</label>
             <div class="col-sm-10">
-            <input type="text" class="form-control <?= ($validation->hasError('nama')) ? 'is-invalid' : '';?>" name="nama" value="<?= old('nama'); ?>">
+            <input type="text" class="form-control <?= ($validation->hasError('nama')) ? 'is-invalid' : '';?>" name="nama" value="<?= $user['nama'] ?>">
             <div class="invalid-feedback">
                 <?= $validation->getError('nama') ;?>
             </div>
@@ -25,7 +26,9 @@
             <div class="col-sm-10">
                 <select class="form-select" aria-label="Default select example" id="kelas" name="kelas">
                     <?php foreach($kelas as $item):?>
-                        <option value="<?=$item['id']?>"><?=$item['nama_kelas']?></option>
+                        <option value="<?=$item['id']?>" <?= $user['id_kelas'] == $item['id'] ? 'selected' : ''?> >
+                        <?= $item['nama_kelas'] ?>
+                        </option>
                     <?php endforeach;?>
                 </select>
             </div>
@@ -33,7 +36,7 @@
         <div class="mb-3 row">
             <label for="npm" class="col-sm-2 col-form-label">NPM</label>
             <div class="col-sm-10">
-                <input class="form-control <?= ($validation->hasError('npm')) ? 'is-invalid' : '';?>" name="npm" value="<?= old('npm'); ?>">
+                <input class="form-control <?= ($validation->hasError('npm')) ? 'is-invalid' : '';?>" name="npm" value="<?= $user['npm'] ?>">
                 <div class="invalid-feedback">
                 <?= $validation->getError('npm') ;?>
             </div>
@@ -42,10 +45,13 @@
         <div class="mb-3 row">
             <div class="col-sm-10">
                 <label> Choose the File to upload: </label>
+                <div style="width: 200px; height: 200px; border-radius: 50%; overflow: hidden; margin: 0 auto; border: 2px solid black;">
+                    <img src="<?= $user['foto'] ?? '<default-foto>' ?>" width= "200px" height= "200px">
+                </div>
                 <input type="file" id="foto" name="foto"/>
             </div>
         </div>
-        <input type="submit" name="tambah_data" value="Tambah Data" class="btn btn-primary">
+        <input type="submit" name="tambah_data" value="Update Data" class="btn btn-primary">
         <a href="/user" class="btn btn-danger">Kembali</a>
     </form>
 </div>
